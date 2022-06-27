5d109cf6912671bb06978b39a6615caf6dcdc383
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2022-06-26 22:48:34 -0300

    styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchList, asyncAddItem, actDeleteItem } from "./actions/list";
 import { updateList } from "./reducers/list";
 
+const btnStyles = "bg-gray-900/50 text-white py-1 px-3 rounded-md";
+
+
 function App() {
   const [inputValue, setInputValue] = useState();
   const dispatch = useDispatch();
@@ -20,6 +23,7 @@ function App() {
     e.preventDefault();
     await dispatch(asyncAddItem({ value: inputValue }))
     dispatch(fetchList());
+    setInputValue("");
   }
 
   const handleRemove = async (id) => {
@@ -27,15 +31,15 @@ function App() {
     dispatch(fetchList());
   }
 
-  return <>
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-      <button>Agregar</button>
+  return <div className="max-w-sm bg-red-500/75 mx-auto p-4 rounded-md">
+    <form onSubmit={handleSubmit} className="flex justify-between">
+      <input className="w-3/4 py-1 px-3 border-none outline-none bg-white/75" type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+      <button className={btnStyles}>Agregar</button>
     </form>
-    <ul>
-      {list.map(item => <li key={item._id}>{item.value}<button onClick={() => handleRemove(item._id)}>X</button></li>)}
+    <ul className="space-y-1 mt-3 p-2 text-white font-bold bg-red-900/50 rounded-sm">
+      {list.map(item => <li className="flex justify-between" key={item._id}>{item.value}<button className={`${btnStyles} font-normal`} onClick={() => handleRemove(item._id)}>X</button></li>)}
     </ul>
-  </>
+  </div>
 };
 
 export default App
